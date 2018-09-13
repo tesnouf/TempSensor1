2889f3fa5d548fef31e590b0b1351642eb57b533
tesnouf <tesnouf@me.com>
2018-09-12 20:20:51 -0700

    Hmm

--- src/buttonDeBounce.cpp
+++ src/buttonDeBounce.cpp
@@ -1,89 +1,237 @@
-// /*
-//   Debounce
-// Looke like this will be the only option to incorporate a buttong into the sketch so that
-// we can have a button of sorts
+// /**
+//  * The MySensors Arduino library handles the wireless radio link and protocol
+//  * between your home built sensors/actuators and HA controller of choice.
+//  * The sensors forms a self healing radio network with optional repeaters. Each
+//  * repeater and gateway builds a routing tables in EEPROM which keeps track of the
+//  * network topology allowing messages to be routed to nodes.
+//  *
+//  * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
+//  * Copyright (C) 2013-2015 Sensnology AB
+//  * Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
+//  *
+//  * Documentation: http://www.mysensors.org
+//  * Support Forum: http://forum.mysensors.org
+//  *
+//  * This program is free software; you can redistribute it and/or
+//  * modify it under the terms of the GNU General Public License
+//  * version 2 as published by the Free Software Foundation.
+//  *
+//  *******************************
+//  *
+//  * REVISION HISTORY
+//  * Version 1.0 - Developed by Bruce Lacey and GizMoCuz (Domoticz)
+//  * Version 1.1 - Modified by hek to incorporate a rotary encode to adjust
+//  *                                light level locally at node
+//  *
+//  * DESCRIPTION
+//  * This sketch provides an example how to implement a dimmable led light node with a rotary
+//  * encoder connected for adjusting light level.
+//  * The encoder has a click button which turns on/off the light (and remembers last dim-level)
+//  * The sketch fades the light (non-blocking) to the desired level.
+//  *
+//  * Default MOSFET pin is 3
+//  *
+//  *  Arduino      Encoder module
+//  *  ---------------------------
+//  *  5V           5V (+)
+//  *  GND          GND (-)
+//  *  4            CLK (or putput 1)
+//  *  5            DT  (or output 1)
+//  *  6            SW (Switch/Click)
+//  */
 //
 //
-//   Each time the input pin goes from LOW to HIGH (e.g. because of a push-button
-//   press), the output pin is toggled from LOW to HIGH or HIGH to LOW. There's a
-//   minimum delay between toggles to debounce the circuit (i.e. to ignore noise).
+// // Enable debug prints
+// #define MY_DEBUG
 //
-//   The circuit:
-//   - LED attached from pin 13 to ground
-//   - pushbutton attached from pin 2 to +5V
-//   - 10 kilohm resistor attached from pin 2 to ground
+// // Enable and select radio type attached
+// #define MY_RADIO_NRF24
+// //#define MY_RADIO_RFM69
 //
-//   - Note: On most Arduino boards, there is already an LED on the board connected
-//     to pin 13, so you don't need any extra components for this example.
+// #include <SPI.h>
+// #include <MySensors.h>
+// #include <Bounce2.h>
+// #include <Encoder.h>
+// #include <Arduino.h>
 //
-//   created 21 Nov 2006
-//   by David A. Mellis
-//   modified 30 Aug 2011
-//   by Limor Fried
-//   modified 28 Dec 2012
-//   by Mike Walters
-//   modified 30 Aug 2016
-//   by Arturo Guadalupi
+// #define LED_PIN 5           // Arduino pin attached to MOSFET Gate pin
+// #define KNOB_ENC_PIN_1 4    // Rotary encoder input pin 1
+// #define KNOB_ENC_PIN_2 7    // Rotary encoder input pin 2
+// #define KNOB_BUTTON_PIN 3   // Rotary encoder button pin
+// #define FADE_DELAY 10       // Delay in ms for each percentage fade up/down (10ms = 1s full-range dim)
+// #define SEND_THROTTLE_DELAY 400 // Number of milliseconds before sending after user stops turning knob
+// #define SN "DimmableLED /w button"
+// #define SV "1.2"
 //
-//   This example code is in the public domain.
+// #define CHILD_ID_LIGHT 1
 //
-//   http://www.arduino.cc/en/Tutorial/Debounce
-// */
+// #define EEPROM_DIM_LEVEL_LAST 1
+// #define EEPROM_DIM_LEVEL_SAVE 2
 //
-// // constants won't change. They're used here to set pin numbers:
-// const int buttonPin = 3;    // the number of the pushbutton pin
-// const int ledPin = 5;      // the number of the LED pin
+// #define LIGHT_OFF 0
+// #define LIGHT_ON 1
 //
-// // Variables will change:
-// int ledState = HIGH;         // the current state of the output pin
-// int buttonState;             // the current reading from the input pin
-// int lastButtonState = LOW;   // the previous reading from the input pin
+// int dimValue;
+// int fadeTo;
+// int fadeDelta;
+// byte oldButtonVal;
+// bool changedByKnob=false;
+// bool sendDimValue=false;
+// unsigned long lastFadeStep;
+// unsigned long sendDimTimeout;
+// char convBuffer[10];
 //
-// // the following variables are unsigned longs because the time, measured in
-// // milliseconds, will quickly become a bigger number than can be stored in an int.
-// unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
-// unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+// MyMessage dimmerMsg(CHILD_ID_LIGHT, V_DIMMER);
+// Encoder knob(KNOB_ENC_PIN_1, KNOB_ENC_PIN_2);
+// Bounce debouncer = Bounce();
 //
-// void setup() {
-//   pinMode(buttonPin, INPUT);
-//   pinMode(ledPin, OUTPUT);
+// void setup()
+// {
+//   // Set knob button pin as input (with debounce)
+//   pinMode(KNOB_BUTTON_PIN, INPUT);
+//   digitalWrite(KNOB_BUTTON_PIN, HIGH);
+//   debouncer.attach(KNOB_BUTTON_PIN);
+//   debouncer.interval(5);
+//   oldButtonVal = debouncer.read();
 //
-//   // set initial LED state
-//   digitalWrite(ledPin, ledState);
+//   // Set analog led pin to off
+//   analogWrite( LED_PIN, 0);
+//
+//   // Retreive our last dim levels from the eprom
+//   fadeTo = dimValue = 0;
+//   byte oldLevel = loadLevelState(EEPROM_DIM_LEVEL_LAST);
+//   Serial.print("Sending in last known light level to controller: ");
+//   Serial.println(oldLevel);
+//   send(dimmerMsg.set(oldLevel), true);
+//
+//   Serial.println("Ready to receive messages...");
 // }
 //
-// void loop() {
-//   // read the state of the switch into a local variable:
-//   int reading = digitalRead(buttonPin);
+// void presentation() {
+//   // Send the Sketch Version Information to the Gateway
+//   present(CHILD_ID_LIGHT, S_DIMMER);
+//   sendSketchInfo(SN, SV);
+// }
 //
-//   // check to see if you just pressed the button
-//   // (i.e. the input went from LOW to HIGH), and you've waited long enough
-//   // since the last press to ignore any noise:
+// void loop()
+// {
+//   // Check if someone turned the rotary encode
+//   checkRotaryEncoder();
 //
-//   // If the switch changed, due to noise or pressing:
-//   if (reading != lastButtonState) {
-//     // reset the debouncing timer
-//     lastDebounceTime = millis();
+//   // Check if someone has pressed the knob button
+//   checkButtonClick();
+//
+//   // Fade light to new dim value
+//   fadeStep();
+// }
+//
+// void receive(const MyMessage &message)
+// {
+//   if (message.type == V_LIGHT) {
+//     // Incoming on/off command sent from controller ("1" or "0")
+//     int lightState = message.getString()[0] == '1';
+//     int newLevel = 0;
+//     if (lightState==LIGHT_ON) {
+//       // Pick up last saved dimmer level from the eeprom
+//       newLevel = loadLevelState(EEPROM_DIM_LEVEL_SAVE);
+//     }
+//     // Send dimmer level back to controller with ack enabled
+//     send(dimmerMsg.set(newLevel), true);
+//     // We do not change any levels here until ack comes back from gateway
+//     return;
+//   } else if (message.type == V_DIMMER) {
+//     // Incoming dim-level command sent from controller (or ack message)
+//     fadeTo = atoi(message.getString(convBuffer));
+//     // Save received dim value to eeprom (unless turned off). Will be
+//     // retreived when a on command comes in
+//     if (fadeTo != 0)
+//       saveLevelState(EEPROM_DIM_LEVEL_SAVE, fadeTo);
+//   }
+//   saveLevelState(EEPROM_DIM_LEVEL_LAST, fadeTo);
+//
+//   Serial.print("New light level received: ");
+//   Serial.println(fadeTo);
+//
+//   if (!changedByKnob)
+//     knob.write(fadeTo);
+//
+//   // Cancel send if user turns knob while message comes in
+//   changedByKnob = false;
+//   sendDimValue = false;
+//
+//   // Stard fading to new light level
+//   startFade();
+// }
+//
+//
+//
+// void checkRotaryEncoder() {
+//   long encoderValue = knob.read();
+//
+//   if (encoderValue > 100) {
+//     encoderValue = 100;
+//     knob.write(100);
+//   } else if (encoderValue < 0) {
+//     encoderValue = 0;
+//     knob.write(0);
 //   }
 //
-//   if ((millis() - lastDebounceTime) > debounceDelay) {
-//     // whatever the reading is at, it's been there for longer than the debounce
-//     // delay, so take it as the actual current state:
+//   if (encoderValue != fadeTo) {
+//     fadeTo = encoderValue;
+//     changedByKnob = true;
+//     startFade();
+//   }
+// }
 //
-//     // if the button state has changed:
-//     if (reading != buttonState) {
-//       buttonState = reading;
+// void checkButtonClick() {
+//   debouncer.update();
+//   byte buttonVal = debouncer.read();
+//   byte newLevel = 0;
+//   if (buttonVal != oldButtonVal && buttonVal == LOW) {
+//     if (dimValue==0) {
+//       // Turn on light. Set the level to last saved dim value
+//       int saved = loadLevelState(EEPROM_DIM_LEVEL_SAVE);
+//       newLevel = saved > 0 ? saved : 100;
+//     }
+//     send(dimmerMsg.set(newLevel),true);
+//   }
+//   oldButtonVal = buttonVal;
+// }
 //
-//       // only toggle the LED if the new button state is HIGH
-//       if (buttonState == HIGH) {
-//         ledState = !ledState;
-//       }
+// void startFade() {
+//   fadeDelta = ( fadeTo - dimValue ) < 0 ? -1 : 1;
+//   lastFadeStep = millis();
+// }
+//
+// // This method provides a graceful none-blocking fade up/down effect
+// void fadeStep() {
+//   unsigned long currentTime  = millis();
+//   if ( dimValue != fadeTo && currentTime > lastFadeStep + FADE_DELAY) {
+//     dimValue += fadeDelta;
+//     analogWrite( LED_PIN, (int)(dimValue / 100. * 255) );
+//     lastFadeStep = currentTime;
+//
+//     Serial.print("Fading level: ");
+//     Serial.println(dimValue);
+//
+//     if (fadeTo == dimValue && changedByKnob) {
+//       sendDimValue = true;
+//       sendDimTimeout = currentTime;
 //     }
 //   }
+//   // Wait a few millisecs before sending in new value (if user still turns the knob)
+//   if (sendDimValue && currentTime > sendDimTimeout + SEND_THROTTLE_DELAY)  {
+//      // We're done fading.. send in new dim-value to controller.
+//      // Send in new dim value with ack (will be picked up in incomingMessage)
+//     send(dimmerMsg.set(dimValue), true); // Send new dimmer value and request ack back
+//     sendDimValue = false;
+//   }
+// }
 //
-//   // set the LED:
-//   digitalWrite(ledPin, ledState);
-//
-//   // save the reading. Next time through the loop, it'll be the lastButtonState:
-//   lastButtonState = reading;
+// // Make sure only to store/fetch values in the range 0-100 from eeprom
+// int loadLevelState(byte pos) {
+//   return min(max(loadState(pos),0),100);
+// }
+// void saveLevelState(byte pos, byte data) {
+//   saveState(pos,min(max(data,0),100));
 // }
--- src/main.cpp
+++ src/main.cpp
@@ -36,9 +36,11 @@ This update has been done as a test - TE
 
  */
 
+
+
 // Enable debug prints
-#define MY_DEBUG
-//#define MY_DEBUG_VERBOSE_RF24
+// #define MY_DEBUG
+// #define MY_DEBUG_VERBOSE_RF24
 
 // Enable and select radio type attached
 #define MY_RADIO_NRF24
@@ -49,13 +51,15 @@ This update has been done as a test - TE
 // #define MY_NODE_ID 3 //prototype
 
 // Enable repeater functionality for this node
-#define MY_REPEATER_FEATURE
+// #define MY_REPEATER_FEATURE
 
 // Include Libraries
 #include <SPI.h>
 #include <MySensors.h>
 #include <DHT.h>
 #include <Arduino.h>
+#include <Bounce2.h>
+#include <Encoder.h>
 
 
 // Set a bunch of stuff for the DHT
@@ -80,17 +84,14 @@ const long ReadingInterval = 60000; // 60secs by default
 
 #define LED_PIN 5      // Arduino pin attached to MOSFET Gate pin
 #define FADE_DELAY 10  // Delay in ms for each percentage fade up/down (10ms = 1s full-range dim)
-
+#define KNOB_BUTTON_PIN 3   // Rotary encoder button pin
 static int16_t currentLevel = 0;  // Current dim level...
 MyMessage dimmerMsg(0, V_DIMMER);
 MyMessage lightMsg(0, V_LIGHT);
 
-// // Pin connected to
-// #define LED_TESTING_PIN 5
-// #define RELAY_1  3  // Arduino Digital I/O pin number for first relay (second on pin+1 etc)
-// #define NUMBER_OF_RELAYS 1 // Total number of attached relays
-// #define RELAY_ON 1  // GPIO value to write to turn on attached relay
-// #define RELAY_OFF 0 // GPIO value to write to turn off attached relay
+
+byte oldButtonVal;
+Bounce debouncer = Bounce();
 
 // A bunch of stuff for the Controller/Gateway
 //prototype
@@ -111,6 +112,29 @@ DHT dht;
 
 
 
+void setup()
+{
+  // Set knob button pin as input (with debounce)
+  pinMode(KNOB_BUTTON_PIN, INPUT);
+  digitalWrite(KNOB_BUTTON_PIN, HIGH);
+  debouncer.attach(KNOB_BUTTON_PIN);
+  debouncer.interval(5);
+  // oldButtonVal = debouncer.read();
+  oldButtonVal = 0; // not sure may switch it about
+  dht.setup(DHT_DATA_PIN); // set data pin of DHT sensor
+  // if (UPDATE_INTERVAL <= dht.getMinimumSamplingPeriod()) {
+  //   Serial.println("Warning: UPDATE_INTERVAL is smaller than supported by the sensor!");
+  // }
+  // Sleep for the time of the minimum sampling period to give the sensor time to power up
+  // (otherwise, timeout errors might occure for the first reading)
+  sleep(dht.getMinimumSamplingPeriod());
+
+  // Set up the LED for use cycle it on and off to ensure harware is working
+  // This occurs at effectively the same time the first message is being sent
+  // pinMode(LED_TESTING_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+  Serial.println("Ready to receive messages...");
+}
 
 void presentation()
 {
@@ -126,26 +150,60 @@ void presentation()
   metric = getControllerConfig().isMetric;
 }
 
-
-void setup()
+/***
+ *  This method provides a graceful fade up/down effect
+ */
+void fadeToLevel( int toLevel )
 {
-  dht.setup(DHT_DATA_PIN); // set data pin of DHT sensor
-  if (UPDATE_INTERVAL <= dht.getMinimumSamplingPeriod()) {
-    Serial.println("Warning: UPDATE_INTERVAL is smaller than supported by the sensor!");
-  }
-  // Sleep for the time of the minimum sampling period to give the sensor time to power up
-  // (otherwise, timeout errors might occure for the first reading)
-  sleep(dht.getMinimumSamplingPeriod());
 
-  // Set up the LED for use cycle it on and off to ensure harware is working
-  // This occurs at effectively the same time the first message is being sent
-  // pinMode(LED_TESTING_PIN, OUTPUT);
-  pinMode(LED_PIN, OUTPUT);
+    int delta = ( toLevel - currentLevel ) < 0 ? -1 : 1;
+
+    while ( currentLevel != toLevel ) {
+        currentLevel += delta;
+        analogWrite( LED_PIN, (int)(currentLevel / 100. * 255) );
+        delay( FADE_DELAY );
+    }
 }
 
 
+void checkButtonClick() {
+  // Serial.println("Button Checked");
+// mostly working lots of radio interfernece and led turns off may need to add in a debounce delay to stop it picking up on the fall??
+  debouncer.update();
+  byte buttonVal = debouncer.read();
+  if (buttonVal == HIGH && currentLevel > 1) {
+    fadeToLevel( 0 );
+  }
+  if (buttonVal == HIGH && currentLevel == 0) {
+    fadeToLevel( 100 );
+  }
+    // Serial.println("Button Pressed");
+  //   if (currentLevel > 1) {
+  //     // current value is > 0 then 0
+  //
+  //     fadeToLevel( 0 );
+  //   } else { // 100
+  //
+  //     fadeToLevel( 100 );
+  //   }
+  // }
+
+  // if (buttonVal != oldButtonVal && buttonVal == LOW) {
+  //   #ifdef MY_DEBUG
+  //   Serial.println("Button Pressed");
+  //   Serial.print(buttonVal);
+  //   Serial.print(oldButtonVal);
+  //   #endif
+  // }
+  // oldButtonVal = buttonVal;
+
+}
+
 void loop()
 {
+// Check if someone has pressed the knob button
+checkButtonClick();
+
 // digitalWrite(LED_BUILTIN, LOW);
 // digitalWrite(LED_TESTING, LOW);
 unsigned long CurrentTempInterval = millis();
@@ -193,29 +251,12 @@ if (CurrentTempInterval - PreviousTempInterval >= ReadingInterval){
 
 }
 
-// void receive(const MyMessage &message) {
-//   // digitalWrite(message.LED_TESTING_PIN, message.getBool()?RELAY_ON:RELAY_OFF);
-//   digitalWrite(LED_TESTING_PIN, message.getBool()?RELAY_ON:RELAY_OFF);
-//   Serial.print("Incoming change for sensor:");
-//   Serial.print(message.sensor);
-//   Serial.print(", New status: ");
-//   Serial.println(message.getBool());
-// }
 
-/***
- *  This method provides a graceful fade up/down effect
- */
-void fadeToLevel( int toLevel )
-{
 
-    int delta = ( toLevel - currentLevel ) < 0 ? -1 : 1;
 
-    while ( currentLevel != toLevel ) {
-        currentLevel += delta;
-        analogWrite( LED_PIN, (int)(currentLevel / 100. * 255) );
-        delay( FADE_DELAY );
-    }
-}
+
+
+
 
 void receive(const MyMessage &message)
 {
